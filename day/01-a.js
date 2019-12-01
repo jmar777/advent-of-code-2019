d9f40cf8ebbc3a5b20dd5e4f34033fdc61836b91
Jeremy Martin <jmar777@gmail.com>
2019-12-01 17:20:23 -0500

    initial commit (with day 1 solution)

---
+++ day/01-a.js
@@ -0,0 +1,8 @@
+import input from '../input/01.txt';
+import _ from 'lodash';
+
+input 
+  |> _.split(?, '\n')
+  |> _.map(?, mass => Math.floor(mass / 3) - 2)
+  |> _.reduce(?, (sum, fuel) => sum + fuel, 0)
+  |> console.log('result:', ?);
---
+++ day/01-b.js
@@ -0,0 +1,12 @@
+import input from '../input/01.txt';
+import _ from 'lodash';
+
+const calculateFuelRecursive = mass =>
+  Math.floor(mass / 3) - 2
+    |> (fuel => fuel >= 0 ? fuel + calculateFuelRecursive(fuel) : Math.max(fuel, 0));
+
+input
+  |> _.split(?, '\n')
+  |> _.map(?, calculateFuelRecursive)
+  |> _.reduce(?, (sum, fuel) => sum + fuel, 0)
+  |> console.log('result:', ?);
